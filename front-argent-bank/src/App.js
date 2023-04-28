f12750e4ce7ad38df20cb224a5f30bc6eb0920dc
Jérôme Dautreme <jerome.dautreme@live.fr>
2023-04-28 15:55:48 +0200

    ajout des composants navigation et footer

--- front-argent-bank/src/App.js
+++ front-argent-bank/src/App.js
@@ -3,15 +3,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './Pages/Home';
 import SignIn from './Pages/SignIn';
 import User from './Pages/User';
+import Navigation from './components/Navigation';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <BrowserRouter>
+         <Navigation/>
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signIn" element={<SignIn />} />
         <Route path="/signIn/user" element={<User />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 };
