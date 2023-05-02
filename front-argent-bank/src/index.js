6037ba0bf5571947bb4246d8e059e3b551fc9d15
Jérôme Dautreme <jerome.dautreme@live.fr>
2023-05-02 10:05:56 +0200

    provider

--- front-argent-bank/src/index.js
+++ front-argent-bank/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './designs/css/main.css';
+// REDUX
+import { Provider } from 'react-redux';
+
 
 const container = document.getElementById('root');
 const root = createRoot(container);
