be539f9cecae63f788088550d47bbe4ccec1aa0d
Jérôme Dautreme <jerome.dautreme@live.fr>
2023-05-04 17:13:55 +0200

    signup page

---
+++ front-argent-bank/src/Pages/SignUp.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import FormSignUp from '../components/form/Form.signUp';
+
+const SignUp = () => {
+    return (
+        <>
+        <main className='main bg-dark'>
+            <FormSignUp />
+        </main>
+        </>
+    );
+};
+
+export default SignUp;
