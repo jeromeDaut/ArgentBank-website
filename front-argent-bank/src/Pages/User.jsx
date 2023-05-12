e6c276c7935c152f4308db180f13665bc0d469f5
Jérôme Dautreme <jerome.dautreme@live.fr>
2023-05-12 10:52:48 +0200

    modification importation loginSuccess

--- front-argent-bank/src/Pages/User.jsx
+++ front-argent-bank/src/Pages/User.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import Account from '../components/account/Account';
 import HeaderUser from '../components/headerUser/headerUser';
 import { useSelector } from 'react-redux';
-import { signIn } from '../app/slice/userTokenSlice';
+import {loginSuccess } from '../app/slice/usersSlice';
 
 const User = () => {
   
   const token = useSelector((state) => state.token);
-  signIn(token)
+  loginSuccess(token)
     return (
         <>
           <main className='main bg-dark'>
