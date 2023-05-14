bb720c42757e34b730a471e63306e8a6ac959d05
Jérôme Dautreme <jerome.dautreme@live.fr>
2023-05-14 09:43:55 +0200

    ajout du firstname dans navigation + style associé

--- front-argent-bank/src/components/navigation/Navigation.jsx
+++ front-argent-bank/src/components/navigation/Navigation.jsx
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { logout } from '../../app/slice/usersSlice';
+import { getProfile } from "../../app/services/getProfile";
 
 
 const Navigation = () => {
   const location = useLocation();
   const isUserPage = location.pathname === "/login/user"; 
   const dispatch = useDispatch();
+  const [firstName, setFirstName] = useState("");
+  
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      getProfile(token)
+        .then((data) => {
+        setFirstName(data.firstName);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }, []);
 
   const handleSignOut = () => {
     dispatch(logout());
-  }
-    return (
+  };
+
+  return (
     <nav className="main-nav">
       <Link className="main-nav-logo" to="/">
         <img
@@ -23,26 +39,26 @@ const Navigation = () => {
         <h1 className="sr-only">Argent Bank</h1>
       </Link>
       <div>
-      {isUserPage ? (
-      <>
-        <Link className="main-nav-item" to="">
-          <i className="fa fa-user-circle"></i>
-          Tony
-        </Link>
-        <Link className="main-nav-item" to="/" onClick={handleSignOut}>
-          <i className="fa fa-sign-out"></i>
-          Sign Out
-        </Link>
-      </>
-    ) : (
-      <Link className="main-nav-item" to="/login" >
-        <i className="fa fa-user-circle"></i>
-        Sign In
-      </Link>
-    )}
+        {isUserPage ? (
+          <>
+            <Link className="main-nav-item" to="">
+              <i className="fa fa-user-circle"></i>
+              <span>{firstName}</span>
+            </Link>
+            <Link className="main-nav-item" to="/" onClick={handleSignOut}>
+              <i className="fa fa-sign-out"></i>
+              Sign Out
+            </Link>
+          </>
+        ) : (
+          <Link className="main-nav-item" to="/login">
+            <i className="fa fa-user-circle"></i>
+            Sign In
+          </Link>
+        )}
       </div>
     </nav>
-    );
+  );
 };
 
 export default Navigation;
