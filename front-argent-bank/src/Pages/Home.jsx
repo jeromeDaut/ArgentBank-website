8a24db25b0c665b7812f1e2c0ffbcb5b226d6b50
Jérôme Dautreme <jerome.dautreme@live.fr>
2023-05-14 09:55:06 +0200

    retour du composant Navigation dans App

--- front-argent-bank/src/Pages/Home.jsx
+++ front-argent-bank/src/Pages/Home.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import Hero from '../components/hero/Hero';
 import Feature from '../components/feature/Feature';
-// import Navigation from '../components/Navigation';
-// import Footer from '../components/Footer';
 
 const Home = () => {
     return (
--- front-argent-bank/src/Pages/User.jsx
+++ front-argent-bank/src/Pages/User.jsx
@@ -1,21 +1,17 @@
 import React from 'react';
 import Account from '../components/account/Account';
 import HeaderUser from '../components/headerUser/headerUser';
-import { useSelector } from 'react-redux';
-import {loginSuccess } from '../app/slice/usersSlice';
 
 const User = () => {
-  
-  const token = useSelector((state) => state.token);
-  loginSuccess(token)
-    return (
-        <>
-          <main className='main bg-dark'>
-          <HeaderUser />
-          <Account />
-          </main>  
-        </>
-    );
+
+  return (
+    <>
+      <main className='main bg-dark'>
+        <HeaderUser  />
+        <Account  />
+      </main>  
+    </>
+  );
 };
 
 export default User;
