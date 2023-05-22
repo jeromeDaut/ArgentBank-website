ff29de8cb33c10ca40f0842e4caff6bc0aea0ef3
Jérôme Dautreme <jerome.dautreme@live.fr>
2023-05-22 09:17:41 +0200

    appel API depuis slice et extraReducers

--- front-argent-bank/src/app/slice/usersSlice.js
+++ front-argent-bank/src/app/slice/usersSlice.js
@@ -1,52 +1,108 @@
 import { createSlice } from "@reduxjs/toolkit"
 import { createAsyncThunk } from "@reduxjs/toolkit";
+import { store } from '../store';
 import axios from "axios";
 
-export const register = createAsyncThunk(
-    'userSlice/register',
+export const login = createAsyncThunk(
+    'userSlice/login',
     async (userData) => {
-      const response = await axios.post("http://localhost:3001/api/v1/user/login", userData);
-      return response.data.body;
+      const {data} = await axios.post("http://localhost:3001/api/v1/user/login", userData);
+      // console.log(data.body)
+      return data.body;
+    }
+  );
+
+export const getProfile = createAsyncThunk(
+    'userSlice/getProfile',
+    async () => {  
+      const token = store.getState().usersReducer.token    
+      const {data} = await axios.post("http://localhost:3001/api/v1/user/profile", {token},
+      { headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    }
+      );
+      //  console.log(data.body)
+      return data.body;
+    }
+  );
+
+  export const PutUserName = createAsyncThunk(
+    'userSlice/PutUserName',
+    async (userName) => { 
+      const token = store.getState().usersReducer.token      
+      const {data} = await axios.put("http://localhost:3001/api/v1/user/profile", {userName},
+      { headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    }
+      );
+       console.log(data)
+      return data;
     }
   );
 
 const initialState={
     token: localStorage.getItem('token') || null,
     isLoggedIn: false,
-    userName: null,
-
-    email: null,
-    password: null,
+    currentUser: {},
     loading: false,
-    error: null
+    error: null,
+    currentUserId: null
 }
 
 const usersSlice= createSlice({
     name:'userSlice',
     initialState,
-    reducers:{
-      loginStart(state) {
-        state.isLoggedIn = false;
-        state.error = null;
-      },
-      loginSuccess(state, action) {
-        state.token = action.payload.token;
-        localStorage.setItem('token', action.payload); // Stockage du token dans le localStorage
-        state.isLoggedIn=true;
-        
-      },
-      loginFailure(state, action) {
-        state.isLoggedIn = false;
-        state.error = action.payload.error;
-      },
+    reducers:{      
       logout(state) {
         state.token = null;
         localStorage.clear();
+        state.isLoggedIn= false;
+        state.currentUser = {};
+        state.loading = false;
         state.error = null;
-        state.isLoggedIn = false;
       },
-    }
+    },
+
+    extraReducers: (builder) => {      
+      builder.addCase(login.fulfilled, (state, action) => {
+        state.token = action.payload.token;
+        localStorage.setItem('token', action.payload.token); // Stockage du token dans le localStorage
+        state.error = null;
+        state.isLoggedIn=true;        
+      })
+
+      builder.addCase(login.rejected, (state, action) => {        
+        localStorage.clear();
+        state.error = action.error.message;
+        console.log(state.error);
+        state.isLoggedIn=false;        
+      })
+
+      builder.addCase(getProfile.fulfilled, (state, action) => {
+        state.currentUser = action.payload;
+        state.currentUserId = action.payload.id
+        console.log(state.currentUser.id);
+        console.log(state.currentUser);
+
+        
+      })
+
+      builder.addCase(PutUserName.fulfilled, (state, action) => {
+         state.currentUser = action.payload.body;
+        // console.log(action.payload.body);
+
+        
+      })
+        
+      
+    },
+      
+    
 })
 
-export const {  loginStart, loginSuccess, loginFailure, logout } = usersSlice.actions;
+export const {  logout } = usersSlice.actions;
 export default usersSlice.reducer
