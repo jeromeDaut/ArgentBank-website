057a46378744f28fc6aab546517cb7f3fec96d15
Jérôme Dautreme <jerome.dautreme@live.fr>
2023-05-23 08:07:04 +0200

    Sémantique

--- front-argent-bank/src/components/hero/Hero.jsx
+++ front-argent-bank/src/components/hero/Hero.jsx
@@ -5,9 +5,9 @@ const Hero = () => {
         <div className="hero">
             <section className="hero-content">
                 <h2 className="sr-only">Promoted Content</h2>
-                <p className="subtitle">No fees.</p>
-                <p className="subtitle">No minimum deposit.</p>
-                <p className="subtitle">High interest rates.</p>
+                <h3 className="subtitle">No fees.</h3>
+                <h3 className="subtitle">No minimum deposit.</h3>
+                <h3 className="subtitle">High interest rates.</h3>
                 <p className="text">Open a savings account with Argent Bank today!</p>
             </section>
         </div>
