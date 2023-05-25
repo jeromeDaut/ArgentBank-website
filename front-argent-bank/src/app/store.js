e9f78b64df797943ab05d14078a7b8007cc4b9f9
Jérôme Dautreme <jerome.dautreme@live.fr>
2023-05-25 07:31:48 +0200

    suppression commentaires

--- front-argent-bank/src/app/store.js
+++ front-argent-bank/src/app/store.js
@@ -1,7 +1,6 @@
 import {configureStore} from '@reduxjs/toolkit';
 import  usersReducer from "./slice/usersSlice";
-// import transactionsReducer from './slice/transactionsSlice';
 
 export const store = configureStore({
-    reducer: { usersReducer, /*transactionsReducer*/},
+    reducer: { usersReducer},
 })
