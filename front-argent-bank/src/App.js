71bc37a39eb7750c41bb892d8dfddedb8996ee47
Jérôme Dautreme <jerome.dautreme@live.fr>
2023-05-25 10:34:50 +0200

    ajout de la route PageNotFound

--- front-argent-bank/src/App.js
+++ front-argent-bank/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from './Pages/Home';
 import Navigation from './components/navigation/Navigation';
 import Footer from './components/footer/Footer';
 import SignIn from './Pages/SignIn';
 import Dashboard from './Pages/Dashboard';
+import PageNotFound from './Pages/PageNotFound';
 
 const App = () => {
+
+  const errorOccurred = false;
 
   return (
     <BrowserRouter>
@@ -15,6 +18,8 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<SignIn />} />
         <Route path="/login/dashboard" element={<Dashboard />} />
+        {errorOccurred && <Navigate to="*" />}
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
       <Footer />
     </BrowserRouter>
