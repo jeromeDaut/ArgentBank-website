5c9f8d7a2d541c996e8d004d80d4b70bce83dc21
Jérôme Dautreme <jerome.dautreme@live.fr>
2023-05-25 10:35:24 +0200

    ajout de la page PageNotFound

---
+++ front-argent-bank/src/Pages/PageNotFound.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+const PageNotFound = () => {
+  return (
+    <main className="error bg-dark">
+      <h1 className="error__title"><span className="error__title--404">Erreur: 404</span></h1>
+      <p>The requested page does not exist.</p>
+      <NavLink to="/">Go back to the homepage ?</NavLink>
+    </main>
+  );
+};
+
+export default PageNotFound;
