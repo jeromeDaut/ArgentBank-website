54a47ceafc8623e500e0f63f0ce83240faed7a67
Jérôme Dautreme <jerome.dautreme@live.fr>
2023-05-25 07:39:11 +0200

    ajout de la logique pour l'affichace du composant account

--- front-argent-bank/src/Pages/Dashboard.jsx
+++ front-argent-bank/src/Pages/Dashboard.jsx
@@ -6,15 +6,45 @@ import { getProfile } from '../app/slice/usersSlice';
 
 const Dashboard = () => {
   const dispatch = useDispatch();
+  
+  
+  const accountData = [
+    {
+      title: 'Argent Bank Checking (x8349)',
+      amount: '$2,082.79',
+      description: 'Available Balance',
+    },
+    {
+      title: 'Argent Bank Savings (x6712)',
+      amount: '$10,928.42',
+      description: 'Available Balance',
+    },
+    {
+      title: 'Argent Bank Credit Card (x8349)',
+      amount: '$184.30',
+      description: 'Current Balance',
+    },
+  ];
 
 useEffect(() => {
   dispatch(getProfile());
 }, [dispatch]);
+
+
   return (
     <>
       <main className='main bg-dark'>
         <HeaderUser  />
-        <Account  />
+        <>
+        {accountData.map((account, index) => (
+        <Account
+          key={index}
+          title={account.title}
+          amount={account.amount}
+          description={account.description}
+        />
+        ))}
+        </>
       </main>  
     </>
   );
